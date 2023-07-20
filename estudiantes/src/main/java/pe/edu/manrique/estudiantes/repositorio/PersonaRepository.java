67ea883a196baca047bce0aa857396626c7d602a
fortueny <erick.manrique112@gmail.com>
2023-07-19 20:49:57 -0500

    Pregunta 02 hecha

--- estudiantes/src/main/java/pe/edu/manrique/estudiantes/repositorio/PersonaRepository.java
+++ estudiantes/src/main/java/pe/edu/manrique/estudiantes/repositorio/PersonaRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.manrique.estudiantes.entidad.Persona;
 
+import java.util.List;
+
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
     // Aquí puedes añadir métodos personalizados si es necesario.
+    List<Persona> findByNombre(String nombre);
 }
